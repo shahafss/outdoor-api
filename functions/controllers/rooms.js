550709de11103a5f910e4fdf1072648c1364a4f8
Shahaf Shumel <shahaflidor@gmail.com>
2020-12-10 23:08:39 +0200

    room model wip

--- functions/controllers/rooms.js
+++ functions/controllers/rooms.js
@@ -1,78 +1,29 @@
+const Room = require("../models/room");
+
 const admin = require("firebase-admin");
-
-var serviceAccount = require("../permissions.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://outdoor-vue.firebaseio.com",
-});
 
 const db = admin.firestore();
 
-exports.getRooms = (req, res) => {
-  (async () => {
-    try {
-      let query = db.collection("rooms");
-      let tempRooms = [];
-      await query.get().then((querySnapshot) => {
-        let docs = querySnapshot.docs;
-        for (let doc of docs) {
-          const roomData = {
-            id: doc.id,
-            title: doc.data().title,
-            category: doc.data().category,
-            date: doc.data().date,
-            description: doc.data().description,
-            participants: doc.data().participants,
-            address: doc.data().address,
-            admin: doc.data().admin,
-            joinedUsers: doc.data().joinedUsers,
-            messages: doc.data().messages,
-            timestamp: doc.data().timestamp,
-          };
-          if (roomData.title !== undefined) tempRooms.push(roomData);
-        }
-      });
-      tempRooms.sort((a, b) => new Date(a.timestamp - b.timestamp)).reverse();
-      return res.status(200).json({ rooms: tempRooms });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+exports.createRoom = (req, res) => {
+  const room = new Room(req.body);
+  room.save((response) => {
+    return res.status(201).send(response);
+  });
 };
 
 exports.getRoom = (req, res) => {
-  console.log("getroom request>>", req.params);
-  (async () => {
-    try {
-      const document = db.collection("rooms").doc(req.params.room_id);
-      let item = await document.get();
-      let response = item.data();
-      return res.status(200).json({ room: response });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+  Room.fetchCurrenRoom(req.params.room_id, (room) => {
+    return res.status(200).json(room);
+  });
 };
 
-exports.createRoom = (req, res) => {
-  console.log("create request>> ", req.body);
-  const room = req.body;
-  (async () => {
-    try {
-      await db
-        .collection("rooms")
-        .add(room)
-        .then((docRef) => {
-          return res.status(201).send({ roomId: docRef.id });
-        });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+exports.getRooms = (req, res) => {
+  Room.fetchAllRooms((rooms) => {
+    if (rooms.length) return res.status(200).json({ rooms: rooms });
+    return res
+      .status(500)
+      .send("connection error. please check your internet connection.");
+  });
 };
 
 exports.updateRoom = (req, res) => {
@@ -88,14 +39,7 @@ exports.updateRoom = (req, res) => {
 };
 
 exports.deleteRoom = (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection("rooms").doc(req.params.room_id);
-      await document.delete();
-      return res.status(200).send("room deleted");
-    } catch (error) {
-      console.log("delete room error >> ", error);
-      return res.status(500).send(error);
-    }
-  })();
+  Room.delete(req.params.room_id, (response) => {
+    return res.status(200).send(response);
+  });
 };
--- functions/models/room.js
+++ functions/models/room.js
@@ -1,15 +1,101 @@
 const admin = require("firebase-admin");
+
+var serviceAccount = require("../permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://outdoor-vue.firebaseio.com",
+});
+
 const db = admin.firestore();
 
 module.exports = class Room {
-  constructor(t) {
-    this.title = t;
+  constructor(room) {
+    this.title = room.title;
+    this.description = room.description;
+    this.category = room.category;
+    this.date = room.date;
+    this.participants = room.participants;
+    this.address = room.address;
+    this.joinedUsers = room.joinedUsers;
+    this.messages = room.messages;
+    this.admin = room.admin;
   }
 
-  // createRoom() {
+  save(callback) {
+    const self = JSON.stringify(this);
+    (async () => {
+      try {
+        await db
+          .collection("rooms")
+          .add(JSON.parse(self))
+          .then((docRef) => {
+            callback({ roomId: docRef.id });
+          });
+      } catch (error) {
+        console.log(error);
+        callback(error);
+      }
+    })();
+  }
 
-  // }
-  static fetchAllRooms() {
-    return;
+  static fetchCurrenRoom(roomId, callback) {
+    (async () => {
+      try {
+        const document = db.collection("rooms").doc(roomId);
+        let item = await document.get();
+        let response = item.data();
+        callback({ room: response });
+      } catch (error) {
+        console.log("getRoom error >> ", error);
+        callback({ err: error });
+      }
+    })();
+  }
+
+  static fetchAllRooms(callback) {
+    (async () => {
+      try {
+        let query = db.collection("rooms");
+        let tempRooms = [];
+        await query.get().then((querySnapshot) => {
+          let docs = querySnapshot.docs;
+          for (let doc of docs) {
+            const roomData = {
+              id: doc.id,
+              title: doc.data().title,
+              category: doc.data().category,
+              date: doc.data().date,
+              description: doc.data().description,
+              participants: doc.data().participants,
+              address: doc.data().address,
+              admin: doc.data().admin,
+              joinedUsers: doc.data().joinedUsers,
+              messages: doc.data().messages,
+              timestamp: doc.data().timestamp,
+            };
+            if (roomData.title !== undefined) tempRooms.push(roomData);
+          }
+        });
+        tempRooms.sort((a, b) => new Date(a.timestamp - b.timestamp)).reverse();
+        callback(tempRooms);
+      } catch (error) {
+        console.log("errrrr>>>>> ", error);
+        callback(error);
+      }
+    })();
+  }
+
+  static delete(roomId, callback) {
+    (async () => {
+      try {
+        const document = db.collection("rooms").doc(roomId);
+        await document.delete();
+        callback("room deleted");
+      } catch (error) {
+        console.log("delete room error >> ", error);
+        callback(error);
+      }
+    })();
   }
 };
